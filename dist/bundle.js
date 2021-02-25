12cb41112deaad47f4fea38eba73be88ff61c7d9
Julian <tipler.julian@gmail.com>
2021-02-24 18:31:00 -0800

    fix further bug with button selecting

--- dist/bundle.js
+++ dist/bundle.js
@@ -25403,8 +25403,6 @@ function __classPrivateFieldSet(receiver, privateMap, value) {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var Tone__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! Tone */ "./node_modules/Tone/build/esm/index.js");
-var _this = undefined;
-
 
 window.addEventListener('DOMContentLoaded', function () {
   //samples column
@@ -25465,8 +25463,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
   generateRows(8);
   document.querySelectorAll(".button-label, .drum-machine-button, .time-button").forEach(function (item) {
+    console.log(item);
     item.addEventListener('click', function () {
-      _this.blur();
+      item.blur();
     });
   });
   document.querySelectorAll(".hidden").forEach(function (el) {
